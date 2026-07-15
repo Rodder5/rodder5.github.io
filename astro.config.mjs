06089c5eb32fbd37f6ba8c41cf93569880b21ae1
Rod <rodneywoods83@gmail.com>
2026-07-14 20:29:12 -0700

    Point site at rodwoods.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rodder5.github.io',
+  site: 'https://rodwoods.dev',
   integrations: [sitemap()],
 });
